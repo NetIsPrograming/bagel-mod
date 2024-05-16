37b098eb547745b32172a18e38cd521d94b4aedc
Net <22157020+NetIsPrograming@users.noreply.github.com>
2024-05-15 23:50:51 -0400

    Switch back to Intelij Idea clean up code, fun stuff.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bagel mod
 A fun mod for me to mess around with fabric.
+Currently in Alpha! 
+Everything has the posability of changing.
---
+++ src/main/java/com/netuserget/bagel/item/HandheldCannonItem.java
@@ -0,0 +1,21 @@
+package com.netuserget.bagel.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class HandheldCannonItem extends Item {
+    public HandheldCannonItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_ANVIL_LAND, 1.0F, 1.0f);
+        return super.use(world, user, hand);
+    }
+}
