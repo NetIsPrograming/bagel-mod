ba06e514c9d9b99095fbddb87ceb4fc2896b28aa
Net <22157020+NetIsPrograming@users.noreply.github.com>
2024-06-12 16:50:00 -0400

    Add Bunny boots

--- src/main/java/com/netuserget/bagel/BagelMod.java
+++ src/main/java/com/netuserget/bagel/BagelMod.java
@@ -1,7 +1,9 @@
 package com.netuserget.bagel;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item.Settings;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,11 +17,14 @@ public class BagelMod implements ModInitializer {
     public static final String MODID = "bagel";
     public static final Logger LOGGER = LoggerFactory.getLogger("bagel");
 
+    public static final ArmorItem BUNNY_BOOTS = new ArmorItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(6)));
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
         LOGGER.info("Its like a everything bagel! (BagelMod Initialization)");
+        Registry.register(Registries.ITEM, new Identifier(MODID, "bunnyboots"), BUNNY_BOOTS);
     }
 }
---
+++ src/main/java/com/netuserget/bagel/mixin/PlayerEntityMixin.java
@@ -0,0 +1,41 @@
+package com.netuserget.bagel.mixin;
+
+import com.netuserget.bagel.BagelMod;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+    private PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
+
+    @Shadow protected abstract void attackLivingEntity(LivingEntity target);
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;updateTurtleHelmet()V"))
+    private void bagel$BunnyBoots(CallbackInfo ci) {
+        updateBunnyBoots();
+    }
+
+    @Unique
+    private void updateBunnyBoots() {
+        ItemStack bootItem = this.getEquippedStack(EquipmentSlot.FEET);
+        if (bootItem.isOf(BagelMod.BUNNY_BOOTS)) {
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 10 * 20, 1, false, false, true));
+        }
+    }
+}
